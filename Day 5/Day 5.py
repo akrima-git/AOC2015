80697e3ea0b36070842e11b0cc7c7f40c681b4cb
PeckishDino <peckishdino@hotmail.com>
2024-12-15 00:17:00 +0000

    Update Day 5.py

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -1,11 +1,5 @@
 import re
 
-p1 = r"a|e|i|o|u"
-p2 = r"(.)\1"
-p3 = r"ab|cd|pq|xy"
-
-p12 = r"(.{2}).*\1"
-p22 = r"(.)\w\1"
 
 def parse(file):
     with open(file, "r") as f:
@@ -13,33 +7,37 @@ def parse(file):
 
 
 def naughty_or_nice(string_input: list):
+    p1 = r"a|e|i|o|u"
+    p2 = r"(.)\1"
+    p3 = r"ab|cd|pq|xy"
     total = 0
     for x in string_input:
         x = x.strip()
         one = re.findall(p1, x)
         two = re.findall(p2, x)
         three = re.findall(p3, x)
-        print(one, two, three)
         if len(one) >= 3 and len(two) >= 1 and len(three) == 0:
             total += 1
-            print("good")
     return total
 
+
 def naughty_or_nice2(string_input: list):
+    p1 = r"(.{2})\w*\1"
+    p2 = r"(.)\w\1"
+
     total = 0
     for x in string_input:
         x = x.strip()
-        one = re.findall(p12, x)
-        two = re.findall(p22, x)
-        print(one, two)
+        one = re.findall(p1, x)
+        two = re.findall(p2, x)
         if len(one) >= 1 and len(two) >= 1:
             total += 1
-            print("good")
     return total
 
+
 def main():
-    print(naughty_or_nice(parse("input.txt")))
-    print(naughty_or_nice2(parse("input.txt")))
+    print("part 1:", naughty_or_nice(parse("input.txt")))
+    print("part 2:", naughty_or_nice2(parse("input.txt")))
 
 
 main()
